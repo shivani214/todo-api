ce1d092c9594010a269e967093d3a52f81deb91c
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-13 14:59:25 +0530

    updating server.js

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 var express = require('express');
-var bodyParser =require('body-parser');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Meet gaurang for lunch',
-    completed: false
-},
-    {
-        id: 2,
-        description: 'Go to market with gaurang',
-        completed: false
+var todos = [//{
+    // id: 1,
+    // description: 'Meet gaurang for lunch',
+    // completed: false
+    //},
+    //  {
+    //    id: 2,
+    //  description: 'Go to market with gaurang',
+    //completed: false
 
-    },
-    {
-        id: 3,
-        description: "Go to home",
-        completed: true
-    } 
-    ];
+    //},
+    //{
+    //  id: 3,
+    //description: "Go to home",
+    //completed: true
+    //} 
+];
 var todoNextId = 1;
-    app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API ROOT');
@@ -36,32 +37,35 @@ app.get('/todos', function (req, res) {
 //Get / todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
 
-    todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, { Id: todoId });
+
     if (matchedTodo) {
         res.json(matchedTodo);
     }
     else {
         res.status(404).send();
-
     }
 });
-app.post('/todos', function (req, res){
-var body =req.body;
+app.post('/todos', function (req, res) {
+    var bodyTemp = req.body;
+var body = _.pick(bodyTemp,'description', 'completed');
 
-body.Id = todoNextId++;
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
 
-todos.push(body);
+    return res.status(400).send();
+    }
+    body.description = body.description.trim();
 
-console.log('description:' + body.description);
 
-res.json(body);
- 
+    body.Id = todoNextId++;
+
+    todos.push(body);
+
+    console.log('description: ' + body.description);
+
+    res.json(body);
+
 });
 
 app.listen(PORT, function () {
