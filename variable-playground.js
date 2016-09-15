18787804ecacb5b72784e0c0a679467995912b00
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-15 13:36:26 +0530

    completed

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+// var person = {
+//     name: 'Sonal',
+//     age: 31
+// };
+//  function updatePerson (obj){
+//      // obj = 
+//      {
+//        //   name: 'Sonal',
+//          // age: 24
+
+//          obj.age = 24;
+//       }; 
+//  }
+//  updatePerson(person);
+//  console.log(person);
+
+// Array Example
+  var grades = [13, 24];
+function addGrades (gradesArr){
+    gradesArr.push(14, 25);
+debugger;
+    }
+
+    addGrades(grades);
+    console.log(grades);
+    
+
+
+ 
