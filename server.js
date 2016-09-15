3b19291eece57ba1ad7f956151801f17452d7696
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-15 15:18:37 +0530

    CRUD Operations done

--- server.js
+++ server.js
@@ -40,7 +40,16 @@ app.get('/todos', function (req, res) {
     else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, { completed: false});
     }
-    res.json(filteredTodos);
+
+
+   if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+       filteredTodos = _.filter(filteredTodos, function (todo){
+           return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+
+       });
+
+   }
+   res.json(filteredTodos);
 });
 //Get / todos/:id
 app.get('/todos/:id', function (req, res) {
