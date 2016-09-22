38175df65b8cd712137acb01dede9af81b9c943c
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-22 16:52:45 +0530

    Convert GET /todos to sequelize again

--- server.js
+++ server.js
@@ -16,32 +16,35 @@ app.get('/', function (req, res) {
 
 //GET / todos
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
+    var query = req.query;
+    var where = {};
 
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, { completed: true });
+    console.log(query.hasOwnProperty('completed'));
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
-    else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, { completed: false });
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            $like: '%' + query.q + '%'
+        };
     }
+    db.todo.findAll({ where: where }).then(function (todos) {
+        res.json(todos);
+    }, function (e) {
+        res.status(500).send();
 
+    });
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-
-        });
-
-    }
-    res.json(filteredTodos);
 });
 //Get / todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    db.todo.findById(todoId).then(function (todo){
+    db.todo.findById(todoId).then(function (todo) {
         if (!!todo) {
-        res.json(todo.toJSON());
+            res.json(todo.toJSON());
         } else {
             res.status(404).send();
         }
@@ -49,8 +52,8 @@ app.get('/todos/:id', function (req, res) {
         res.status(500).send();
 
     });
-    });
-    
+});
+
 app.post('/todos', function (req, res) {
     var bodyTemp = req.body;
     var body = _.pick(bodyTemp, 'description', 'completed');
@@ -62,7 +65,7 @@ app.post('/todos', function (req, res) {
 
     });
 
-    });
+});
 
 
 app.delete('/todos/:id', function (req, res) {
