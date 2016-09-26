511144dfb6ef47807729ef11fbb8441d220b11ce
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-26 13:33:10 +0530

    Add login route and authenticate class method.

--- db.js
+++ db.js
@@ -8,17 +8,17 @@ if (env === 'production') {
     });
 
 } else {
-  sequelize = new Sequelize(undefined, undefined, undefined, {
-  'dialect': 'sqlite',
-  'storage': __dirname + '/data/dev-todo-api.sqlite'
- });
-    
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+
 }
-var db={};
+var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.user = sequelize.import(__dirname + '/models/user.js');
-db.sequelize =sequelize;
-db.Sequelize =Sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports =db;
+module.exports = db;
--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
-var bcryptjs= require('bcryptjs');
+var bcryptjs = require('bcryptjs');
 var _ = require('underscore');
 
 
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('user', {
+    var user = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -22,14 +22,13 @@ module.exports = function (sequelize, DataTypes) {
             validate: {
                 len: [7, 100]
             },
-            set: function (value){
-var salt = bcryptjs.genSaltSync(10);
-var hashPassword = bcryptjs.hashSync(value, salt);
-     this.setDataValue('password', value);
-this.setDataValue('salt', salt);
-this.setDataValue('password_hash', hashPassword);    
-       }
-
+            set: function (value) {
+                var salt = bcryptjs.genSaltSync(10);
+                var hashPassword = bcryptjs.hashSync(value, salt);
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashPassword);
+            }
         }
     }, {
             hooks: {
@@ -39,11 +38,36 @@ this.setDataValue('password_hash', hashPassword);
                     }
                 }
             },
-            instanceMethods:{
+            classMethods: {
+                authenticate: function (body) {
+                    return new Promise(function (resolve, reject) {
+                        if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+                            return reject();
+                        }
+                        user.findOne({
+                            where: {
+                                email: body.email
+                            }
+                        }).then(function (user) {
+                            if (!user || !bcryptjs.compareSync(body.password, user.get('password_hash'))) {
+
+                                return reject();
+                            }
+
+                            resolve(user);
+
+                        }, function (e) {
+                            reject();
+                        });
+                    })
+                }
+            },
+            instanceMethods: {
                 toPublicJSON: function () {
                     var json = this.toJSON();
                     return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
                 }
             }
         });
+    return user;
 };
