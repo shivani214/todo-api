4b9ccea0afb3b6d9682150f12d897686bde57cac
Shivani Gupta <helloshivani21apr@gmail.com>
2016-09-27 15:15:09 +0530

    Convert CRUD request to use current user todos only

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/', function (req, res) {
 //GET / todos
 app.get('/todos', middleware.requireAuthentication, function (req, res) {
     var query = req.query;
-    var where = {};
+    var where = {
+        userId: req.user.get('id')
+    };
 
     console.log(query.hasOwnProperty('completed'));
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
@@ -43,7 +45,12 @@ app.get('/todos', middleware.requireAuthentication, function (req, res) {
 //Get / todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+        where: {
+            id: todoId,
+            userId: req.user.get('id')
+        }
+    }).then(function (todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -60,12 +67,12 @@ app.post('/todos', middleware.requireAuthentication, function (req, res) {
     var body = _.pick(bodyTemp, 'description', 'completed');
 
     db.todo.create(body).then(function (todo) {
-      // res.json(todo.toJSON());
-      req.user.addTodo(todo).then(function (){
-return todo.reload();
-      }).then(function (todo) {
-          res.json(todo.toJSON());
-      });
+        // res.json(todo.toJSON());
+        req.user.addTodo(todo).then(function () {
+            return todo.reload();
+        }).then(function (todo) {
+            res.json(todo.toJSON());
+        });
     }, function (e) {
         res.status(400).json(e);
 
@@ -79,7 +86,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function (req, res) {
 
     db.todo.destroy({
         where: {
-            id: todoId
+            id: todoId,
+            userId: req.user.get('id')
         }
     }).then(function (rowsDeleted) {
         if (rowsDeleted === 0) {
@@ -109,7 +117,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function (req, res) {
     if (body.hasOwnProperty('description')) {
         attributes.description = body.description;
     }
-    db.todo.findById(todoId).then(function (todo) {
+    db.todo.findOne({
+       where: { 
+           id: todoId,
+        userId: req.user.get('id')
+       }
+     }).then(function (todo) {
         if (todo) {
             todo.update(attributes).then(function () {
                 res.json(todo.toJSON());
